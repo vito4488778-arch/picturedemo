c0ca2cf23e601e74a723187086984255914348ac
vito <vito4488778@gmail.com>
2025-09-12 09:26:15 +0800

    Initial Commit

---
+++ picturedemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  picturedemo
+//
+//  Created by 114iosClassStudent04 on 2025/9/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ picturedemo/picturedemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  picturedemoApp.swift
+//  picturedemo
+//
+//  Created by 114iosClassStudent04 on 2025/9/12.
+//
+
+import SwiftUI
+
+@main
+struct picturedemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
